8a40c3a7b44ba3ed4ee79eb292bb6e7974e97bbe
Evgeniy123 <osnovadota23@mail.ru>
2023-12-10 14:36:46 +0300

    Доработал 8.py

--- 2023.11.30/8.py
+++ 2023.11.30/8.py
@@ -3,6 +3,12 @@ fb1 = fb2 = 1
 x = 0
 
 while x < number:
+    print(fb2, end=' ')
     fb1,fb2 = fb2,fb1 + fb2
     x += 1
-    print(fb2, end=' ')    
+
+# 11
+# 1 2 3 5 8 13 21 34 55 89 144       
+
+# 1
+# 1 
