68e9675789b375df0c618144d0e3dd7d0b4541a0
Evgeniy123 <osnovadota23@mail.ru>
2023-12-11 22:44:04 +0300

    Добавил 4.py

---
+++ 2023.11.30/4.py
@@ -0,0 +1,21 @@
+number = int(input())
+score = 0
+
+for num in range(10**(number-1),10**number):
+    if num > 1:
+        for i in range(2,num):  
+            if num % i == 0:
+                break
+        else:
+            score += 1
+
+print(score)
+
+# 3
+# 143
+
+# 4
+# 1061
+
+# 2
+# 21
